050999baa27719f1e7747b6ccc1456ccd57891b3
Patrik Kraif <kraifpatrik@gmail.com>
2019-05-28 18:53:22 +0200

    YYC cache directory as command line param (for GMS1.4)

--- yyc-overwrite.py
+++ yyc-overwrite.py
@@ -2,6 +2,7 @@ import getpass
 import json
 import os
 import re
+import sys
 from shutil import copyfile
 
 REGEX_STACKTRACE_LINE = r"YY_STACKTRACE_LINE\((\d+)\);\n?"
@@ -92,7 +93,8 @@ def inject_native_types(types, string):
         string = re.sub(r"{}\.as\w+\(\)".format(name_cpp), name_cpp, string)
 
         if type_ == "bool":
-            string = re.sub(r"BOOL_RValue\(.*{}[^)]*\)".format(name_cpp), name_cpp, string)
+            string = re.sub(
+                r"BOOL_RValue\(.*{}[^)]*\)".format(name_cpp), name_cpp, string)
 
         original = "YYRValue local_{};".format(name)
         new = "{static}{const}{unsigned}{type} local_{name}{val};".format(
@@ -106,14 +108,21 @@ def inject_native_types(types, string):
     return string
 
 
+def cpp_is_script(file):
+    return file[:11] == "gml_Script_"
+
+
+def cpp_is_event(file):
+    return file[:11] == "gml_Object_"
+
+
 def reconstruct_gml_path(build_bff, cpp_path):
     project_dir = build_bff.get_project_dir()
     path_src = ""
-    prefix = cpp_path[:11]
     is_script = False
 
     # Reconstruct path of object event
-    if prefix == "gml_Object_":
+    if cpp_is_event(cpp_path):
         split = cpp_path.split("_")
         file_name = split[-2:]
         if file_name[0] != "PreCreate":
@@ -123,7 +132,7 @@ def reconstruct_gml_path(build_bff, cpp_path):
             path_src = os.path.join(project_dir, "objects",
                                     object_name, file_name)
     # Reconstruct path of script
-    elif prefix == "gml_Script_":
+    elif cpp_is_script(cpp_path):
         is_script = True
         name = file[11:-8]
         path_src = os.path.join(project_dir, "scripts",
@@ -192,6 +201,100 @@ def inject_blocks(string, blocks):
     return result
 
 
+def process_cpp(file, build=None, path_cpp=None):
+    if file[-8:] != ".gml.cpp":
+        return
+
+    if build:
+        path_cpp = build.get_cpp_dir()
+
+    if path_cpp:
+        path_dest = os.path.join(path_cpp, file)
+    else:
+        raise "One build or path_cpp must be defined!"
+
+    try:
+        path_src, is_script = reconstruct_gml_path(build, file)
+    except:
+        path_src = ""
+        is_script = cpp_is_script(file)
+
+    print("Processing", path_dest)
+
+    # Read C++ blocks from GML
+    cpp_blocks = []
+    cpp_types = []
+    try:
+        with open(path_src) as f:
+            fcontent = f.read()
+            cpp_blocks = get_blocks(fcontent, "/*cpp", "*/")
+            cpp_types = get_native_types(fcontent)
+    except:
+        print("No GML found")
+
+    # No C++ block found
+    # if not cpp_blocks and not cpp_types:
+    #     print("Skipping", path_dest, "(no modifications found)")
+    #     return
+
+    # Overwrite C++
+    with open(path_dest) as f:
+        func_name = file[:-8]
+        signature = "{}{}".format(
+            "YYRValue& " if is_script else "void ", func_name)
+        code_cpp = f.read()
+        func_start = code_cpp.find(signature)
+
+        if func_start == -1:
+            print("Function {} not found, skipping!".format(func_name))
+            return
+
+        func_start = code_cpp.find("{", func_start + len(signature))
+        func_end = code_cpp.rfind("}")
+
+        body = code_cpp[func_start+1:func_end]
+        body_new = body
+
+        prefix = ""
+        suffix = ""
+
+        if cpp_blocks:
+            prefix = "\n"
+            suffix = "\n"
+
+            block_first = cpp_blocks[0]
+            block_first_code = block_first["code"]
+            overwrite_prefix = "-overwrite"
+            overwrite = block_first_code.startswith(overwrite_prefix)
+
+            if overwrite:
+                block_first["code"] = block_first_code[len(
+                    overwrite_prefix):].lstrip()
+                if is_script:
+                    prefix = "\nYY_STACKTRACE_FUNC_ENTRY(\"{}\", 0);\n_result = 0;\n".format(
+                        func_name)
+                    suffix = "\nreturn _result;\n"
+                body_new = "\n".join(
+                    list(map(lambda b: b["code"], cpp_blocks)))
+            else:
+                try:
+                    body_new = inject_blocks(body, cpp_blocks)
+                except Exception as e:
+                    print(e)
+                    return
+
+        if cpp_types:
+            body_new = inject_native_types(cpp_types, body_new)
+
+        body_new = remove_stacktrace_lines(body_new)
+
+        new_code = code_cpp[:func_start+1] + prefix + \
+            body_new + suffix + code_cpp[func_end:]
+
+    with open(path_dest, "w") as f:
+        f.write(new_code)
+
+
 class BuildBff:
     """ A wrapper around build.bff file, providing getters to its properties,
     as well as its default path on the disk.
@@ -235,46 +338,52 @@ class BuildBff:
 
 
 if __name__ == "__main__":
-    # Load or create config
-    save_conf = False
+    if len(sys.argv) > 1:
+        # Load cache directory from command line (no injection, only cleanup,
+        # handy for GMS1.4)
+        build = None
+        path_cpp = sys.argv[1]
+    else:
+        # Load or create config
+        save_conf = False
 
-    try:
-        with open("config.json") as file:
-            config = json.load(file)
-        build_bff_dir = config["build_bff"]
-        print("Loaded config.json")
-    except:
-        save_conf = True
-        config = {}
+        try:
+            with open("config.json") as file:
+                config = json.load(file)
+            build_bff_dir = config["build_bff"]
+            print("Loaded config.json")
+        except:
+            save_conf = True
+            config = {}
 
-    if not "build_bff" in config or not build_bff_dir:
-        default = BuildBff.PATH_DEFAULT
-        build_bff_dir = input(
-            "Enter path to the build.bff file [{}]: ".format(default))
-        if not build_bff_dir:
-            build_bff_dir = default
-        config["build_bff"] = build_bff_dir
-        save_conf = True
+        if not "build_bff" in config or not build_bff_dir:
+            default = BuildBff.PATH_DEFAULT
+            build_bff_dir = input(
+                "Enter path to the build.bff file [{}]: ".format(default))
+            if not build_bff_dir:
+                build_bff_dir = default
+            config["build_bff"] = build_bff_dir
+            save_conf = True
 
-    if save_conf:
-        with open("config.json", "w") as file:
-            json.dump(config, file, indent=2)
-        print("Saved config")
+        if save_conf:
+            with open("config.json", "w") as file:
+                json.dump(config, file, indent=2)
+            print("Saved config")
 
-    # Load build.bff
-    try:
-        build = BuildBff(build_bff_dir)
-        print("Loaded build.bff")
-    except:
-        print("ERROR: Could not load build.bff!")
-        exit(1)
+        # Load build.bff
+        try:
+            build = BuildBff(build_bff_dir)
+            print("Loaded build.bff")
+        except:
+            print("ERROR: Could not load build.bff!")
+            exit(1)
 
-    # Project info
-    path_cpp = build.get_cpp_dir()
+        # Project info
+        path_cpp = build.get_cpp_dir()
+        print("Project:", build.get_project_name())
+        print("Config:", build.get_config())
+        print("Project directory:", build.get_project_dir())
 
-    print("Project:", build.get_project_name())
-    print("Config:", build.get_config())
-    print("Project directory:", build.get_project_dir())
     print("Target directory:", path_cpp)
 
     # Check for permission
@@ -284,86 +393,6 @@ if __name__ == "__main__":
 
     # Inject C++
     for file in os.listdir(path_cpp):
-        path_dest = os.path.join(path_cpp, file)
-        path_src, is_script = reconstruct_gml_path(build, file)
-
-        # File is not a script or event
-        if not path_src:
-            continue
-
-        # Read C++ blocks from GML
-        cpp_blocks = []
-        cpp_types = []
-        try:
-            with open(path_src) as f:
-                fcontent = f.read()
-                cpp_blocks = get_blocks(fcontent, "/*cpp", "*/")
-                cpp_types = get_native_types(fcontent)
-        except:
-            continue
-
-        # No C++ block found
-        # if not cpp_blocks and not cpp_types:
-        #     print("Skipping", path_dest, "(no modifications found)")
-        #     continue
-
-        # Overwrite C++
-        print("Processing", path_dest)
-
-        with open(path_dest) as f:
-            func_name = file[:-8]
-            signature = "{}{}".format(
-                "YYRValue& " if is_script else "void ", func_name)
-            code_cpp = f.read()
-            func_start = code_cpp.find(signature)
-
-            if func_start == -1:
-                print("Function {} not found, skipping!".format(func_name))
-                continue
-
-            func_start = code_cpp.find("{", func_start + len(signature))
-            func_end = code_cpp.rfind("}")
-
-            body = code_cpp[func_start+1:func_end]
-            body_new = body
-
-            prefix = ""
-            suffix = ""
-
-            if cpp_blocks:
-                prefix = "\n"
-                suffix = "\n"
-
-                block_first = cpp_blocks[0]
-                block_first_code = block_first["code"]
-                overwrite_prefix = "-overwrite"
-                overwrite = block_first_code.startswith(overwrite_prefix)
-
-                if overwrite:
-                    block_first["code"] = block_first_code[len(
-                        overwrite_prefix):].lstrip()
-                    if is_script:
-                        prefix = "\nYY_STACKTRACE_FUNC_ENTRY(\"{}\", 0);\n_result = 0;\n".format(
-                            func_name)
-                        suffix = "\nreturn _result;\n"
-                    body_new = "\n".join(
-                        list(map(lambda b: b["code"], cpp_blocks)))
-                else:
-                    try:
-                        body_new = inject_blocks(body, cpp_blocks)
-                    except Exception as e:
-                        print(e)
-                        continue
-
-            if cpp_types:
-                body_new = inject_native_types(cpp_types, body_new)
-
-            body_new = remove_stacktrace_lines(body_new)
-
-            new_code = code_cpp[:func_start+1] + prefix + \
-                body_new + suffix + code_cpp[func_end:]
-
-        with open(path_dest, "w") as f:
-            f.write(new_code)
+        process_cpp(file, build=build, path_cpp=path_cpp)
 
     print("Finished")
